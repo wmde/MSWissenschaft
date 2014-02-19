446da3f6f74edab0ed0caf2e336c64cd08c2c13b
Johannes Kroll <jkroll@lavabit.com>
2014-02-19 18:52:44 +0100

    split pier name into city and address

--- dbimport/dbimport.py
+++ dbimport/dbimport.py
@@ -45,14 +45,16 @@ def create_category_table(cursor, tablename):
                          )""" % tablename)
 
 def create_pier_table(cursor, tablename):
+                        #~ pier_name VARBINARY(255),
     cursor.execute("""CREATE TABLE %s (
-                        pier_name VARBINARY(255),
+                        pier_city VARBINARY(255),
+                        pier_address VARBINARY(255),
                         pier_id INT,
                         pier_latitude DOUBLE,
                         pier_longitude DOUBLE,
                         pier_date_start VARBINARY(10),
                         pier_date_end VARBINARY(10),
-                        UNIQUE KEY(pier_name),
+                        UNIQUE KEY(pier_city, pier_address),
                         UNIQUE KEY(pier_id),
                         KEY(pier_date_start),
                         KEY(pier_date_end)
@@ -92,12 +94,13 @@ def importpois():
     for file in os.listdir(POIDIR):
         if not file.endswith('.csv'): continue
         print("processing file %s" % file)
-        m= re.match('([0-9]{2})-(.*)-(.*).csv', file)
+        m= re.match('([0-9]{2})\.([^\.]*)\.(.*)\.(.*).csv', file)
         pierindex= m.group(1)
-        piername= m.group(2)
-        mapcategory= m.group(3)
+        piercity= m.group(2)
+        pieraddress= m.group(3)
+        mapcategory= m.group(4)
         
-        piers[pierindex]= piername
+        piers[pierindex]= (piercity, pieraddress)
         if mapcategory in mapcategories:
             categoryindex= mapcategories.index(mapcategory)
         else:
@@ -108,12 +111,15 @@ def importpois():
         with open(os.path.join(POIDIR, file)) as f:
             reader= csv.DictReader(f)   #, ("page_title", "latitude", "longitude", "hitcount"))
             for row in reader:
+                if row['page_title'].strip()=='':
+                    print("skipping blank line...")
+                    continue
                 try:
                     cursor.execute("INSERT INTO %s_new (page_title, latitude, longitude, hitcount, pier_id, category_id)" % SQL_POITABLE +
                                    "VALUES(%s, %s, %s, %s, %s, %s)", (row['page_title'], row['latitude'], row['longitude'], row['hitcount'], pierindex, categoryindex))
                 except MySQLdb.IntegrityError as e:
                     if e.args[0]==1062:
-                        print("ignoring duplicate entry for '%s' in category '%s' at pier '%s'" % (row['page_title'], mapcategory, piername))
+                        print("ignoring duplicate entry for '%s' in category '%s' at pier '%s'" % (row['page_title'], mapcategory, piercity))
                     else:
                         raise
     
@@ -123,7 +129,7 @@ def importpois():
 
     overwrite_table(create_pier_table, cursor, "%s_new" % SQL_PIERTABLE)
     for i in piers:
-        cursor.execute('INSERT INTO %s_new' % SQL_PIERTABLE + '(pier_name, pier_id) VALUES (%s, %s)', (piers[i], i))
+        cursor.execute('INSERT INTO %s_new' % SQL_PIERTABLE + '(pier_city, pier_address, pier_id) VALUES (%s, %s, %s)', (piers[i][0], piers[i][1], i))
     
     print("reading pier info")
     with open(PIERDESCFILE) as f:
@@ -132,9 +138,9 @@ def importpois():
             m= re.match('([0-9]+)\.([0-9]+)\.([0-9]+)', d.strip())
             return '%04s-%02s-%02s' % (m.group(3), m.group(2), m.group(1))
         for row in reader:
-            cmd= ('UPDATE %s_new ' % SQL_PIERTABLE) + "SET pier_latitude=%s, pier_longitude=%s, pier_date_start=%s, pier_date_end=%s WHERE pier_name LIKE %s"
+            cmd= ('UPDATE %s_new ' % SQL_PIERTABLE) + "SET pier_latitude=%s, pier_longitude=%s, pier_date_start=%s, pier_date_end=%s WHERE pier_city=%s"
             #~ print(cmd)
-            cursor.execute(cmd, (float(row['Breite']), float(row['Laenge']), fmtdate(row['Datum Ankunft']), fmtdate(row['Datum Abreise']), row['Station']+'-%'))
+            cursor.execute(cmd, (float(row['Breite']), float(row['Laenge']), fmtdate(row['Datum Ankunft']), fmtdate(row['Datum Abreise']), row['Station']))
     
     print("renaming tables")
     move_table(cursor, SQL_POITABLE+'_new', SQL_POITABLE)
