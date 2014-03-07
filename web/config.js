2962201063316a7aa60799668b457f7db933db20
Johannes Kroll <jkroll@lavabit.com>
2014-03-07 13:44:57 +0100

    add crap

--- web/config.js
+++ web/config.js
@@ -13,20 +13,25 @@ config= {
 		[ "Beginn Sp&auml;tkaiserzeit", 180 ],
 		[ "Beginn Sp&auml;tantike", 284 ],
 	],
-    "POIBase": "../db/pois.py",
+    "POIBase": "../query/query.py",
     "POILayers": [
         {
-            "title": "<img src='../img/icon-turm-haekchen.png' style='margin: 4px;' align='right'> Befestigungsanlagen (Daten gesichert)",
-            "ranges": "verified",
+            "title": "<img src='img/icon-Politik.png' style='margin: 4px;' align='right'> Politik",
+            "ranges": "verified",   // maybe i have to change all this... "ranges"?
             "visible": true
         },
         {
-            "title": "<img src='../img/icon-turm-transp.png' style='margin: 4px;' align='right'> Befestigungsanlagen (ungesichert/gesch&auml;tzt)",
+            "title": "<img src='img/icon-Kultur.png' style='margin: 4px;' align='right'> Kultur",
+            "ranges": "verified",   // maybe i have to change all this... "ranges"?
+            "visible": true
+        },
+        {
+            "title": "<img src='img/icon-Bildung.png' style='margin: 4px;' align='right'> Bildung",
             "ranges": "unverified",
             "visible": true
         },
         {
-            "title": "<img src='../img/icon-turm-kreuz.png' style='margin: 4px;' align='right'> Befestigungsanlagen (in diesem Jahr nicht existierend)",
+            "title": "<img src='img/icon-Wirtschaft.png' style='margin: 4px;' align='right'> Wirtschaft",
             "ranges": "inverse",
             "visible": false
         }
