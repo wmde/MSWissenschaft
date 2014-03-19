36a0de5a11d1befa4fab56d0a15601d32f98bdeb
Johannes Kroll <jkroll@lavabit.com>
2014-03-19 16:00:13 +0100

    setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -98,6 +98,7 @@ clone_repo() {
 }
 
 install_xfce_config() {
+    sudo -u $APPUSR sh -c "mkdir -p $(appuserdir)/.config" || exit 1
     sudo -u $APPUSR sh -c "rm -rf $(appuserdir)/.config/xfce4 && cp -r $(appdir)/setup/dotconfig/xfce4 $(appuserdir)/.config/xfce4" || exit 1
     sudo -u $APPUSR sh -c "rm -rf $(appuserdir)/.config/autostart && cp -r $(appdir)/setup/dotconfig/autostart $(appuserdir)/.config/autostart" || exit 1
 }
